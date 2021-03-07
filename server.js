b875883dd1c746b8bd67f99414f3a453bfe41452
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-07 13:01:32 +0100

    Set the templating engine and added middleware to parse the request body

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const axios = require('axios');
 
 const app = express();
 
+// Set the templating engine
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+
+// Body parser middleware
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello everyone from backend!'));
 
 app.listen('3000', () => console.log('Server started at port 3000'));
