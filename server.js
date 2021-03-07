f5b5ea5bf18a2c17f90eaffd1584c0f12ed7b015
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-07 17:03:15 +0100

    Added a post route using form input to get weather data from OpenWeather API

--- server.js
+++ server.js
@@ -10,7 +10,30 @@ app.engine('handlebars', exphbs({ defaultLayout: false }));
 
 // Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.render('index'));
 
-app.listen('3000', () => console.log('Server started at port 3000'));
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  const API_KEY = require('./sources/keys.json').API_KEY;
+
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`,
+    )
+    .then(function (response) {
+      res.render('index', {
+        weatherText: `The temperature in ${cityName} is ${response.data.main.temp}`,
+      });
+    })
+    .catch(function (error) {
+      res.render('index', {
+        weatherText: `City not found`,
+      });
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
