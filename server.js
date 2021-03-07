5101412eea71cac45c28e7458b899efa7fd2c12c
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-07 12:49:59 +0100

    Added a function to get route to sen response

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/', () => console.log('Hello everyone from backend!'));
+app.get('/', (req, res) => res.send('Hello everyone from backend!'));
 
 app.listen('3000', () => console.log('Server started at port 3000'));
