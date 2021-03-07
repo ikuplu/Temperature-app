420b2751d4e9fdb4953dee17fe4652207b70941b
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-07 13:17:33 +0100

    Added a handlebars template inside a views folder which is rendered by the get route

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.engine('handlebars', exphbs({ defaultLayout: false }));
 // Body parser middleware
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello everyone from backend!'));
+app.get('/', (req, res) => res.render('index'));
 
 app.listen('3000', () => console.log('Server started at port 3000'));
